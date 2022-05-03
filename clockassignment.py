c9f9864996ba4976825e030582b471866863d8cc
05taniasanchez <93358591+05taniasanchez@users.noreply.github.com>
2022-05-02 20:18:12 -0700

    Add files via upload

---
+++ clockassignment.py
@@ -0,0 +1,18 @@
+from tkinter import*
+from tkinter.ttk import*
+from time import strftime
+
+root = Tk()
+root.title("clock")
+root.resizable(0,0)
+
+def time():
+    string = strftime("%I:%M:%S %p")
+    lbl.config(text= string)
+    lbl.after(1000,time)
+
+lbl = Label(root, font= ("ds-digital", 90), background= "blue", foreground= "White")
+lbl.pack(anchor = "center")
+time()
+
+mainloop()
